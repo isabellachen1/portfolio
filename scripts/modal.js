f71483db93902c5b8c19e19b3f5b698ad3f4ad17
Isabella Chen <143982520+isabellachen1@users.noreply.github.com>
2024-06-09 01:52:42 +0000

    Edits

---
+++ scripts/modal.js
@@ -0,0 +1,12 @@
+
+$("#contact-button").click(function() {
+    $("#modal").removeClass("hidden");
+    $("#close-modal").removeClass("hidden");
+
+  });
+
+$("#close-modal").click(function() {
+    $("#modal").addClass("hidden");
+    $("#close-modal").addClass("hidden");
+
+  });
