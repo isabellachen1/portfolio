f7048999f5f371436cefffb3e7430fbb55ac2169
Isabella Chen <isabellac1116@gmail.com>
2024-08-19 19:45:47 -0400

    Updates on homepage

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,31 +1,31 @@
 $(document).ready(function() {
-    adjustMenuVisibility();
-  
-    $(window).resize(adjustMenuVisibility);
-  
-    function adjustMenuVisibility() {
-        if ($(window).width() <= 600) {
-            $("#nav-menu ul li").addClass("hidden");
-            $("#close-menu").addClass("hidden");
-            $("#menu-button").removeClass("hidden");
-        } else {
-            $("#menu-button").addClass("hidden");
-            $("#close-menu").addClass("hidden");
-            $("#nav-menu ul li").removeClass("hidden");
-        }
-    }
-  
-  $("#menu-button").click(function() {
-    $("#menu-button").addClass("hidden");
-    $("#close-menu").removeClass("hidden");
-    $("#nav-menu ul li").removeClass("hidden");
-  });
-  
-  $("#close-menu").click(function() {
-    $("#nav-menu ul li").addClass("hidden");
-    $("#close-menu").addClass("hidden");
-    $("#menu-button").removeClass("hidden");
-  });
-  
-  });
-  
+  adjustMenuVisibility();
+
+  $(window).resize(adjustMenuVisibility);
+
+  function adjustMenuVisibility() {
+      if ($(window).width() <= 600) {
+          $("#nav-menu ul li").addClass("hidden");
+          $("#close-menu").addClass("hidden");
+          $("#menu-button").removeClass("hidden");
+      } else {
+          $("#menu-button").addClass("hidden");
+          $("#close-menu").addClass("hidden");
+          $("#nav-menu ul li").removeClass("hidden");
+      }
+  }
+
+$("#menu-button").click(function() {
+  $("#menu-button").addClass("hidden");
+  $("#close-menu").removeClass("hidden");
+  $("#nav-menu ul li").removeClass("hidden");
+});
+
+$("#close-menu").click(function() {
+  $("#nav-menu ul li").addClass("hidden");
+  $("#close-menu").addClass("hidden");
+  $("#menu-button").removeClass("hidden");
+});
+
+});
+
