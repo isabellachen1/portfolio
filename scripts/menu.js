2ebbc80e36dfc5cea3e13e0f3b0d3046f65f7dec
Isabella Chen <isabellac1116@gmail.com>
2024-08-27 20:34:15 -0400

    Add projects page

--- scripts/menu.js
+++ scripts/menu.js
@@ -4,14 +4,16 @@ $(document).ready(function() {
   $(window).resize(adjustMenuVisibility);
 
   function adjustMenuVisibility() {
-      if ($(window).width() <= 600) {
+      if ($(window).width() <= 800) {
           $("#nav-menu ul li").addClass("hidden");
           $("#close-menu").addClass("hidden");
           $("#menu-button").removeClass("hidden");
+          $(".initial").removeClass("hidden");
       } else {
           $("#menu-button").addClass("hidden");
           $("#close-menu").addClass("hidden");
           $("#nav-menu ul li").removeClass("hidden");
+          $(".initial").removeClass("hidden");
       }
   }
 
@@ -19,12 +21,15 @@ $("#menu-button").click(function() {
   $("#menu-button").addClass("hidden");
   $("#close-menu").removeClass("hidden");
   $("#nav-menu ul li").removeClass("hidden");
+  $(".initial").addClass("hidden");
 });
 
 $("#close-menu").click(function() {
   $("#nav-menu ul li").addClass("hidden");
   $("#close-menu").addClass("hidden");
   $("#menu-button").removeClass("hidden");
+  $(".initial").removeClass("hidden");
+
 });
 
 });
