e88edff8e68f83408fdd8f7c097e18a93d2933e3
Isabella Chen <isabellac1116@gmail.com>
2024-08-29 15:28:29 -0400

    New changes and sections

--- scripts/menu.js
+++ scripts/menu.js
@@ -4,33 +4,42 @@ $(document).ready(function() {
   $(window).resize(adjustMenuVisibility);
 
   function adjustMenuVisibility() {
-      if ($(window).width() <= 800) {
-          $("#nav-menu ul li").addClass("hidden");
-          $("#close-menu").addClass("hidden");
-          $("#menu-button").removeClass("hidden");
-          $(".initial").removeClass("hidden");
-      } else {
-          $("#menu-button").addClass("hidden");
-          $("#close-menu").addClass("hidden");
-          $("#nav-menu ul li").removeClass("hidden");
-          $(".initial").removeClass("hidden");
-      }
+    if ($(window).width() <= 800) {
+      $("#nav-menu ul li").addClass("hidden");
+      $("#close-menu").addClass("hidden");
+      $("#menu-button").removeClass("hidden");
+      $(".initial").removeClass("hidden");
+    } else {
+      $("#menu-button").addClass("hidden");
+      $("#close-menu").addClass("hidden");
+      $("#nav-menu ul li").removeClass("hidden");
+      $(".initial").removeClass("hidden");
+    }
   }
 
-$("#menu-button").click(function() {
-  $("#menu-button").addClass("hidden");
-  $("#close-menu").removeClass("hidden");
-  $("#nav-menu ul li").removeClass("hidden");
-  $(".initial").addClass("hidden");
+  $("#menu-button").click(function() {
+    $("#menu-button").addClass("hidden");
+    $("#close-menu").removeClass("hidden");
+    $("#nav-menu ul li").removeClass("hidden");
+    $(".initial").addClass("hidden");
+  });
+
+  $("#close-menu").click(function() {
+    closeMenu();
+  });
+
+  // Automatically close the menu when a nav link is clicked
+  $("#nav-menu ul li a").click(function() {
+    if ($(window).width() <= 800) {
+      closeMenu();
+    }
+  });
+
+  // Function to close the menu
+  function closeMenu() {
+    $("#nav-menu ul li").addClass("hidden");
+    $("#close-menu").addClass("hidden");
+    $("#menu-button").removeClass("hidden");
+    $(".initial").removeClass("hidden");
+  }
 });
-
-$("#close-menu").click(function() {
-  $("#nav-menu ul li").addClass("hidden");
-  $("#close-menu").addClass("hidden");
-  $("#menu-button").removeClass("hidden");
-  $(".initial").removeClass("hidden");
-
-});
-
-});
-
